7dd25211dcef24acf2521a38c8b6517973fbd277
Jaewon Kim <jwkim@berkeley.edu>
2025-04-09 23:29:24 -0700

    player and enemy unit movement v1

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EnemyMovement : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody;
+    private Rigidbody2D myRigidBody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
